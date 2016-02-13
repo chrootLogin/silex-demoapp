67625ca64b50d2c12f190dfb5c5404ff15979ebf
Simon Erhardt <me@rootlogin.ch>
2016-02-13 13:55:21 +0100

    changed text

--- web/index_dev.php
+++ web/index_dev.php
@@ -2,7 +2,7 @@
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['SERVER_ADDR'], array('10.10.10.10'))
+    || !in_array(@$_SERVER['SERVER_ADDR'], array('10.11.11.12'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
